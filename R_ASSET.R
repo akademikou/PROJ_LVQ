433c24a34da5d3f657e170356393d5d8512218df
akademikou <akademikou@gmail.org>
2023-05-27 23:32:25 +0300

    OK

--- R_ASSET.R
+++ R_ASSET.R
@@ -11,7 +11,7 @@ dev_height <- 768
 dev_width  <- 9
 dev_height <- 7
 dev_unit <- "in"
-while (!is.null(dev.list())){dev.off()}
+# while (!is.null(dev.list())){dev.off()}
 
 library(xts)
 library(lubridate)
@@ -26,7 +26,7 @@ source("~/R_FUNC/func_CI.R")
 source("~/R_FUNC/func_auxM3.R")
 
 
-PAR <- GET_PAR()
+PAR  <- GET_PAR()
 BNB  <- READ_WITH_TIME("ASSET_BNB.txt")
 BTC  <- READ_WITH_TIME("ASSET_BTC.txt")
 ETH  <- READ_WITH_TIME("ASSET_ETH.txt")
@@ -37,32 +37,148 @@ BTC  <- xts(BTC,order.by = as.POSIXct(index(BTC)))
 ETH  <- xts(ETH,order.by = as.POSIXct(index(ETH)))
 XRP  <- xts(XRP,order.by = as.POSIXct(index(XRP)))
 
-BNB$Close <- BNB$Close/coredata(BNB$Close[[1]])
-BTC$Close <- BTC$Close/coredata(BTC$Close[[1]])
-ETH$Close <- ETH$Close/coredata(ETH$Close[[1]])
-XRP$Close <- XRP$Close/coredata(XRP$Close[[1]])
+BNB[,1:4] <- BNB[,1:4]/coredata(BNB$Close[[1000]])
+BTC[,1:4] <- BTC[,1:4]/coredata(BTC$Close[[1000]])
+ETH[,1:4] <- ETH[,1:4]/coredata(ETH$Close[[1000]])
+XRP[,1:4] <- XRP[,1:4]/coredata(XRP$Close[[1000]])
 
+F_NAME_1 <- "VERI_BNB/EMIR_TAM.txt"
+F_NAME_2 <- "VERI_BTC/EMIR_TAM.txt"
+F_NAME_3 <- "VERI_ETH/EMIR_TAM.txt"
+F_NAME_4 <- "VERI_XRP/EMIR_TAM.txt"
+
+CIZ1   <- xts(numeric(nrow(BNB)),order.by = as.POSIXct(index(BNB))); names(CIZ1)<-c('EMIR')
+CIZ2   <- xts(numeric(nrow(BTC)),order.by = as.POSIXct(index(BTC))); names(CIZ2)<-c('EMIR')
+CIZ3   <- xts(numeric(nrow(ETH)),order.by = as.POSIXct(index(ETH))); names(CIZ3)<-c('EMIR')
+CIZ4   <- xts(numeric(nrow(XRP)),order.by = as.POSIXct(index(XRP))); names(CIZ4)<-c('EMIR')
+
+tmp  <- read.table(F_NAME_1,header = FALSE);  CIZ1$EMIR <- tmp[,1]
+tmp  <- read.table(F_NAME_2,header = FALSE);  CIZ2$EMIR <- tmp[,1]
+tmp  <- read.table(F_NAME_3,header = FALSE);  CIZ3$EMIR <- tmp[,1]
+tmp  <- read.table(F_NAME_4,header = FALSE);  CIZ4$EMIR <- tmp[,1]
+###################################################################################
 if(1){
   C<-data.frame(index(BNB))
   C<-format(C,'%Y/%m/%d')
-  T<-seq(1, nrow(C), by = nrow(C)/10)
+  T<-seq(0, nrow(C), by = 1000)
   
-  SINIR=c(min(BNB$Close,BTC$Close,ETH$Close,XRP$Close),
-          max(BNB$Close,BTC$Close,ETH$Close,XRP$Close))
+  RENK =c("black","green","red",'blue')
+  
+  WALLET_1 <- ORDER_EVALUATE(coredata(CIZ1$EMIR),BNB$Close,PAR$TxnFees)
+  WALLET_2 <- ORDER_EVALUATE(coredata(CIZ2$EMIR),BTC$Close,PAR$TxnFees)
+  WALLET_3 <- ORDER_EVALUATE(coredata(CIZ3$EMIR),ETH$Close,PAR$TxnFees)
+  WALLET_4 <- ORDER_EVALUATE(coredata(CIZ4$EMIR),XRP$Close,PAR$TxnFees)
+  
+  # SINIR_x=c(1,5876)
+  SINIR_1=c(min(BNB$Close,BTC$Close,ETH$Close,XRP$Close),
+            max(BNB$Close,BTC$Close,ETH$Close,XRP$Close))
+  
+  SINIR_2=c(min(WALLET_1[,5],WALLET_2[,5],WALLET_3[,5],WALLET_4[,5]),
+            max(WALLET_1[,5],WALLET_2[,5],WALLET_3[,5],WALLET_4[,5]))
   
   dev.new(width = dev_width, height = dev_height, unit = dev_unit,noRStudioGD = TRUE)
-  plot.new()
-  par(mar = c(15, 4, 4, 4) + 0.3)  # Leave space for z axis
-  matplot(data.frame(BNB$Close,BTC$Close,ETH$Close,XRP$Close),  axes=FALSE,
-          type=c('l','l','l','l'),
-          col =c("black","green","red",'blue'),
-          lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n')
+
+  par(mfrow = c(2, 1),     # 2x2 layout
+      oma = c(5, 4, 0, 0)+0.1, # two rows of text at the outer left and bottom margin
+      mar = c(0, 0, 3, 3)+0.1, # space for one row of text at ticks and to separate plots
+      mgp = c(2, 1, 0))    # axis label at 2 rows distance, tick labels at 1 row
+      # xpd = NA)            # allow content to protrude into outer margin (and beyond)
   
-  grid(nx = 0, ny = NULL, col = "red", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
-  axis(side=2, ylim=SINIR, col="red",col.axis="red",las=1)
-  # axis(side=1,at=T,labels=C[T,1],las=2)
-  # axis(side=1,at=T,labels = FALSE)
+  matplot(data.frame(BNB$Close,BTC$Close,ETH$Close,XRP$Close),  axes=FALSE,
+        type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',ylim=SINIR_1,
+        xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "gray", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_1, col="black",col.axis="black",las=1)
+  abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
+  box()
+  legend("topleft",legend=c("BNB","BTC",'ETH','XRP'),lwd=3,col=RENK)
+  title(xlab = "time",
+        ylab = "normalized asset price",
+        outer = TRUE, line = 3)
+  ###################################################################################
+  matplot(data.frame(WALLET_1[,5],WALLET_2[,5],WALLET_3[,5],WALLET_4[,5]),  axes=FALSE,
+        type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',ylim=SINIR_2,
+        xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "gray", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_2, col="black",col.axis="black",las=1)
   text(x=T,  par("usr")[3],labels = C[T,1], srt = 70, pos = 1,offset = 2, xpd = TRUE)
   abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
   box()
+  legend("topleft",legend=c("ret_BNB","ret_BTC",'ret_ETH','ret_XRP'),lwd=3,col=RENK)
+  ###################################################################################
+
+}
+if(1){
+###################################################################################
+  dev.new(width = dev_width, height = dev_height, unit = dev_unit,noRStudioGD = TRUE)
+  par(mfrow = c(2,2),
+      oma = c(5,4,0,0) + 0.1,
+      mar = c(0,0,3,3) + 0.1)
+  ###################################################################################
+  matplot(data.frame(BNB$Close,1+WALLET_1[,5]),  axes=FALSE,
+          type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',
+          xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "red", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_1, col="red",col.axis="red",las=1)
+  abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
+  box()
+  legend("topleft",legend=c("BNB","ret_BNB"),lwd=3,col=RENK)
+  ###################################################################################
+  matplot(data.frame(BTC$Close,1+WALLET_2[,5]),  axes=FALSE,
+          type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',
+          xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "red", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_1, col="red",col.axis="red",las=1)
+  abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
+  box()
+  legend("topleft",legend=c("BTC","ret_BTC"),lwd=3,col=RENK)
+  ###################################################################################
+  matplot(data.frame(ETH$Close,1+WALLET_3[,5]),  axes=FALSE,
+          type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',
+          xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "red", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_1, col="red",col.axis="red",las=1)
+  abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
+  box()
+  legend("topleft",legend=c("ETH","ret_ETH"),lwd=3,col=RENK)
+  ###################################################################################
+  matplot(data.frame(XRP$Close,1+WALLET_4[,5]),  axes=FALSE,
+          type=c('l','l','l','l'),col =RENK,lwd =c(1,1,1,1),lty =c(1,1,1,1), ylab='',xaxt='n',
+          xaxs = "i", yaxs = "i")
+  grid(nx = 0, ny = NULL, col = "red", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+  axis(side=2, ylim=SINIR_1, col="red",col.axis="red",las=1)
+  abline( v=T, col="gray", lty=3)
+  abline( v=1000, col="gray", lty=3,lwd=3)
+  box()
+  legend("topleft",legend=c("XRP","ret_XRP"),lwd=3,col=RENK)
+  ###################################################################################
+}
+###################################################################################
+
+if(0){
+  info1 <- TRADE_PERFORMANCE_TRADE(BNB,coredata(CIZ1$EMIR),PAR)
+  title(main = F_NAME_1, sub = "Sub-title", xlab = "X axis", ylab = "Y axis",
+        cex.main = 2,   font.main= 4, col.main= "red",
+        cex.sub = 0.75, font.sub = 3, col.sub = "green", col.lab ="darkblue"
+  )
+  info2 <- TRADE_PERFORMANCE_TRADE(BTC,coredata(CIZ2$EMIR),PAR)
+  title(main = F_NAME_2, sub = "Sub-title", xlab = "X axis", ylab = "Y axis",
+        cex.main = 2,   font.main= 4, col.main= "red",
+        cex.sub = 0.75, font.sub = 3, col.sub = "green", col.lab ="darkblue"
+  )
+  info3 <- TRADE_PERFORMANCE_TRADE(ETH,coredata(CIZ3$EMIR),PAR)
+  title(main = F_NAME_3, sub = "Sub-title", xlab = "X axis", ylab = "Y axis",
+        cex.main = 2,   font.main= 4, col.main= "red",
+        cex.sub = 0.75, font.sub = 3, col.sub = "green", col.lab ="darkblue"
+  )
+  info4 <- TRADE_PERFORMANCE_TRADE(XRP,coredata(CIZ4$EMIR),PAR)
+  title(main = F_NAME_4, sub = "Sub-title", xlab = "X axis", ylab = "Y axis",
+        cex.main = 2,   font.main= 4, col.main= "red",
+        cex.sub = 0.75, font.sub = 3, col.sub = "green", col.lab ="darkblue"
+  )
 }
--- R_MAK_LVQ_2.R
+++ R_MAK_LVQ_2.R
@@ -12,10 +12,8 @@ dev_width  <- 9
 dev_height <- 7
 dev_unit <- "in"
 # while (!is.null(dev.list())){dev.off()}
-
 # git remote set-url origin https://github.com/akademikou/PROJ_LVQ.git
-usethis::use_git_config(user.name="akademikou", 
-                        user.email="akademikou@gmail.org")
+# usethis::use_git_config(user.name="akademikou",user.email="akademikou@gmail.org")
 
 library(xts)
 library(lubridate)
@@ -23,32 +21,40 @@ library(dplyr)     # lead lag
 library(class)     # LVQ
 library(caret)     # confusionMatrix
 library(quantmod)
-source("~/R_FUNC/func_BEST.R")
-source("~/R_FUNC/func_FILE.R")
-source("~/R_FUNC/func_BACKTEST.R")
-source("~/R_FUNC/func_CI.R")
-source("~/R_FUNC/func_auxM3.R")
-#PSO <- read.table("RUN_XRP/PSO_OPT.txt",header = FALSE)
-PSO <- read.table("PSO_OPT.txt",header = FALSE)
-SIL <- read.table("old_PSO_OPT.txt",header = FALSE)
-SIL <- rbind(SIL,PSO)
+source('~/R_FUNC/func_BEST.R')
+source('~/R_FUNC/func_FILE.R')
+source('~/R_FUNC/func_BACKTEST.R')
+source('~/R_FUNC/func_CI.R')
+source('~/R_FUNC/func_auxM3.R')
+
+F_NAME_1 <- 'PSO_OPT.txt'
+F_NAME_2 <- 'old_PSO_OPT.txt'
+F_NAME_3 <- 'ASSET_BTC.txt'
+F_NAME_4 <- 'EMIR_TAM.txt'
+F_NAME_5 <- 'EMIR_RUN_'
+
+PSO <- read.table(F_NAME_1,header = FALSE); EN <- ncol(PSO)
+SIL <- read.table(F_NAME_2,header = FALSE)
+SIL <- rbind(SIL[,1:(EN-3)],PSO[,1:(EN-3)])
 SIL <- SIL[!duplicated(SIL), ]
-write.table(SIL, file = "old_PSO_OPT.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+write.table(SIL, file = F_NAME_2, sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 print('OK')
+# stop()
 ########################################################################
 PAR <- GET_PAR()
-TI  <- READ_WITH_TIME("ASSET_TI.txt")
+TI  <- READ_WITH_TIME(F_NAME_3)
 TI  <- xts(TI,order.by = as.POSIXct(index(TI)))
-TI$Open  <- TI$Open /coredata(TI$Close[[1000]])
-TI$High  <- TI$High /coredata(TI$Close[[1000]])
-TI$Low   <- TI$Low  /coredata(TI$Close[[1000]])
-TI$Close <- TI$Close/coredata(TI$Close[[1000]])
+TI$Open  <- TI$Open /coredata(TI$Close[[1001]])
+TI$High  <- TI$High /coredata(TI$Close[[1001]])
+TI$Low   <- TI$Low  /coredata(TI$Close[[1001]])
+TI$Close <- TI$Close/coredata(TI$Close[[1001]])
 
-ENB=max(abs(TI$PCD  )); print(ENB); TI$PCD  <-(TI$PCD   *100/ENB)
-ENB=max(abs(TI$EMAC )); print(ENB); TI$EMAC <-(TI$EMAC  *100/ENB)
-ENB=max(abs(TI$RSIC )); print(ENB); TI$RSIC <-(TI$RSIC  *100/ENB)
-ENB=max(abs(TI$ATR  )); print(ENB); TI$ATR  <-(TI$ATR   *100/ENB)
-ENB=max(abs(TI$STOCH)); print(ENB); TI$STOCH<-(TI$STOCH *100/ENB)
+# ENB=max(abs(TI$PCD  )); print(ENB); TI$PCD  <-(TI$PCD   *100/ENB)
+# ENB=max(abs(TI$EMAC )); print(ENB); TI$EMAC <-(TI$EMAC  *100/ENB)
+# ENB=max(abs(TI$RSIC )); print(ENB); TI$RSIC <-(TI$RSIC  *100/ENB)
+# ENB=max(abs(TI$ATR  )); print(ENB); TI$ATR  <-(TI$ATR   *100/ENB)
+# ENB=max(abs(TI$STOCH)); print(ENB); TI$STOCH<-(TI$STOCH *100/ENB)
 
 df  <- data.frame(lag(coredata(TI$PCD  ),0), lag(coredata(TI$PCD  ),1), lag(coredata(TI$PCD  ),2),
                   lag(coredata(TI$EMAC ),0), lag(coredata(TI$EMAC ),1), lag(coredata(TI$EMAC ),2),
@@ -62,13 +68,13 @@ df[is.na(df)] <-0
 
 
 
-LIM<-data.frame(matrix(0,nrow=ncol(df),ncol=4))
-for(i in 1:ncol(df)){
-  LIM[i,1]<-max(PSO[,i])
-  LIM[i,2]<-min(PSO[,i])
-  LIM[i,3]<-max(df [,i])
-  LIM[i,4]<-min(df [,i])
-}
+# LIM<-data.frame(matrix(0,nrow=ncol(df),ncol=4))
+# for(i in 1:ncol(df)){
+#   LIM[i,1]<-max(PSO[,i])
+#   LIM[i,2]<-min(PSO[,i])
+#   LIM[i,3]<-max(df [,i])
+#   LIM[i,4]<-min(df [,i])
+# }
 # stop()
 # AD<-names(TI)
 # for(i in 1:ncol(TI)){print(c(AD[i],round(min(TI[,i]),2),round(max(TI[,i]),2)))}
@@ -85,8 +91,6 @@ nPSS <- 5
 nH   <- (3*nPSS)
 ################################################################################
 # C++ EMIRLERI ILE ANALIZ
-
-
 if(0){
   DEL <- 100
   for(line in 1:(nrow(PSO))){
@@ -98,7 +102,7 @@ if(0){
     if(u2>nrow(TI)){break}
     print(c(ilk,son,a1,u1,a2,u2))
     
-    NAME <- paste('EMIR_RUN_',toString(line-1),'.txt',sep='')
+    NAME <- paste(F_NAME_5,toString(line-1),'.txt',sep='')
     tmp  <- read.table(NAME,header = FALSE)
 
     CIZ1$EMIR <- 0
@@ -117,26 +121,18 @@ if(0){
   title('EMIR_RUN_TEST')
  # stop()
 }
-if(0){
-  NAME <- paste('EMIR_TAM.txt',sep='')
-  tmp  <- read.table(NAME,header = FALSE)
-  CIZ4$EMIR <- tmp[,1]
-  
-  # CIZ4$EMIR <- lead(coredata(CIZ4$EMIR),1); CIZ4$EMIR[is.na(CIZ4$EMIR)] <-0
-  
-  info <- TRADE_PERFORMANCE_TRADE(TI,coredata(CIZ4$EMIR),PAR)
-  
-  title(main = "EMIR_TAM_TEST", sub = "Sub-title",
-        xlab = "X axis", ylab = "Y axis",
-        cex.main = 2,   font.main= 4, col.main= "red",
-        cex.sub = 0.75, font.sub = 3, col.sub = "green",
-        col.lab ="darkblue"
-  )
-  # info <- TRADE_PERFORMANCE_CALCULATIONS(TI,coredata(CIZ4$EMIR),PAR)
+################################################################################
+if(1){
+  tmp  <- read.table(F_NAME_4,header = FALSE);  
+  CIZ1$EMIR <- tmp[,1]
+  # CIZ1$EMIR <- lag(coredata(CIZ1$EMIR),1); CIZ1$EMIR[is.na(CIZ1$EMIR)] <-0
+  PAR$TxnFees <- 0.005
+  info <- TRADE_PERFORMANCE_TRADE(TI,coredata(CIZ1$EMIR),PAR)
+  title(F_NAME_4, col.main='blue', cex.main=2, font.main=4, adj=0, line=0)
 }
 ################################################################################
 # LVQ PSO ILE ANALIZ
-if(1){
+if(0){
   DEL <- 100
   CIZ4$EMIR <- 0;
   D    <- numeric(nH);
@@ -149,7 +145,6 @@ if(1){
 
     print(c(a2,u2))
     W    <- (PSO[line,1:(ncol(PSO)-3)]);
-    
     for(k in a2:u2){
       if(k>nrow(df)){break;}
       ind  <- 1;
@@ -180,17 +175,15 @@ if(1){
   }
   CIZ4$EMIR[nrow(CIZ4)]  <- 0
   # CIZ4$EMIR <- ORDER_KON(CIZ4$EMIR,TI$RSI,TI$Close,son)
+  # info <- TRADE_PERFORMANCE_CALCULATIONS(TI,coredata(CIZ4$EMIR),PAR)
   info <- TRADE_PERFORMANCE_TRADE(TI,coredata(CIZ4$EMIR),PAR)
-  title('CALC_C_TEST', col.main='blue', cex.main=2, font.main=4, adj=0, line=0)
-  
+  YAZI <- paste('CALC_C_TEST\n',F_NAME_1)
+  title(YAZI, col.main='blue', cex.main=2, font.main=4, adj=0, line=0)
   # col.main: Changed title font color to blue.
   # cex.main: Increased title font to twice the default size.
   # font.main: Changed title font style to italic.
   # adj: Moved title all the way to the left.
   # line: Moved title down to touch the top of the plot.
-  
-  
-   # info <- TRADE_PERFORMANCE_CALCULATIONS(TI,coredata(CIZ4$EMIR),PAR)
 }
 ########################################################################
 warnings()
